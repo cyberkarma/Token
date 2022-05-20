1474386be64a035e9ed46f57c47db6a2bca848c7
artemkhodzhayan <a.khojayan@cyrm.ru>
2022-05-20 11:58:54 +0500

    new tests and tasks

--- test/index.ts
+++ test/index.ts
@@ -9,7 +9,7 @@ describe("Token contract", function () {
   let owner : Signer;
   let addr1 : Signer;
   let addr2 : Signer;
-  const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+  const ZERO_ADDRESS = ethers.constants.AddressZero;
 
   beforeEach(async function () {
       Token = await ethers.getContractFactory("ERC20");
@@ -37,5 +37,40 @@ describe("Token contract", function () {
       it("Should check what you can cut token", async function () {
         expect(await newToken.decimals()).to.equal(0);
       })
+
+      it("SHOULD BE REVERTED | try mint to zero address", async function() {
+        await expect(newToken.connect(owner).mint(ZERO_ADDRESS, 200))
+          .to.be.revertedWith("Cant mint to zero address")
+      })
+  
+      it("SHOULD BE REVERTED | try burn from zero address", async function() {
+        await expect(newToken.connect(owner).burn(ZERO_ADDRESS, 200))
+          .to.be.revertedWith("Cant burn from zero address")
+      })
+
+      it("SHOULD BE REVERTED | try burn out of balance", async function() {
+        await expect(newToken.connect(owner).burn(addr1.getAddress(), 200))
+          .to.be.revertedWith("Amount out of balance")
+      })  
+  
+      it("SHOULD BE REVERTED | not owner try mint or burn", async function() {
+        await expect(newToken.connect(addr1).burn(owner.getAddress(), 200))
+          .to.be.revertedWith("You are not an owner")
+  
+        await expect(newToken.connect(addr1).mint(owner.getAddress(), 200))
+          .to.be.revertedWith("You are not an owner")
+      })
+  
+      it("SHOULD BE SUCCESS  | mint and burn change balance", async function() {
+        await newToken.connect(owner).mint(owner.getAddress(), 200)
+        let balance = await newToken.connect(owner).balanceOf(owner.getAddress())
+        expect(balance).to.be.eq(200)
+  
+        await newToken.connect(owner).burn(owner.getAddress(), 100)
+        balance = await newToken.connect(owner).balanceOf(owner.getAddress())
+        expect(balance).to.be.eq(100)
+      })
+
+     
     });
 })
